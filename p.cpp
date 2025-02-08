16fe0941fea650a0d083c9f8c10b615f6fb9cd8f
Davit <dav.isoyan.0211@gmail.com>
2025-02-08 06:08:31 +0400

    with coments + isbn

--- p.cpp
+++ p.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
+#include <cstdlib> 
+#include <cstring> // for comparing
 
 struct Book
 {
@@ -21,9 +21,35 @@ void addBook(Book** lib, int* size, int* n)
     std::cin.ignore(); 
     std::cin.getline((*lib)[*size].title, 100);
     std::cin.getline((*lib)[*size].author, 100);
-    std::cin.getline((*lib)[*size].isbn, 20);
+    char test[20];   //test for isbn input
+    bool flag=true;
+    while(flag)
+    {
+        flag=false;
+        std::cin.getline(test, 20);   //input of isbn version
+        for(int i=0; i<(*size); ++i)
+        {
+            if(strcmp(test, (*lib)[i].isbn) == 0)    // if the version exists try again
+            {
+                std::cout<<"Try again"<<std::endl;
+                flag=true;
+                break;
+            }        
+        }
+        if(!flag)   // if the version is new
+        {
+            for(int i=0; ; ++i)
+            {
+                if(test[i] == '\0')
+                {
+                    break;
+                }
+                (*lib)[*size].isbn[i]=test[i];  // accept the version
+            }
+        }
+    }
     (*lib)[*size].available=1;
-    ++(*size);
+    ++(*size);  
 }
 
 void listBooks(Book* lib, const int size)
@@ -40,7 +66,7 @@ void searchBook(Book* lib, const int size, char* sp)
     bool flag=false;
     for(int i=0; i<size; ++i)
     {
-        if (strcmp(lib[i].title, sp) == 0 || strcmp(lib[i].author, sp) == 0)
+        if (strcmp(lib[i].title, sp) == 0 || strcmp(lib[i].author, sp) == 0)    //if such an author or title exists, output the data for that book
         {
             flag=true;
             std::cout<<lib[i].title<<" "<<lib[i].author<<" "<<lib[i].isbn<<" "<<lib[i].available<<std::endl;
@@ -58,13 +84,13 @@ void borrowBook(Book* lib, const int size, char* index)
     bool flag=false;
     for(int i=0; i<size; ++i)
     {
-        if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 1)
+        if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 1)    // if there is such a book, and it is available
         {
             flag=true;
             lib[i].available=0;
             break;
         }
-        else if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 0)
+        else if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 0)   // if there is such a book, and it is not available
         {
             flag=true;
             std::cout<<"The book is missing from the library."<<std::endl;
@@ -82,13 +108,13 @@ void returnBook(Book* lib, const int size, char* index)
     bool flag=false;
     for(int i=0; i<size; ++i)
     {
-        if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 0)
+        if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 0)    // if there is such a book, and it is not available
         {
             flag=true;
             lib[i].available=1;
             break;
         }
-        else if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 1)
+        else if(strcmp(lib[i].isbn, index) == 0 && lib[i].available == 1)   // if there is such a book, and it is available
         {
             flag=true;
             std::cout<<"The book is not missing from the library."<<std::endl;
@@ -115,7 +141,7 @@ int main()
         std::cin>>lib[i].title>>lib[i].author>>lib[i].isbn;
         lib[i].available=1;
     }
-    char t[10];
+    char t[10]; // for option input
     std::cout<<"input option (add , list , search , borrow , return or exit)"<<std::endl;
     std::cin>>t;
     while (strcmp(t, "exit") != 0)
@@ -130,21 +156,21 @@ int main()
         }
         else if(strcmp(t, "search") == 0)
         {
-            char sp[30];
+            char sp[30];    // searching author or title
             std::cin.ignore();
             std::cin.getline(sp, 100);
             searchBook(lib, size, sp);
         }
         else if(strcmp(t, "borrow") == 0)
         {
-            char index[20];
+            char index[20];     // searching isbn
             std::cout<<"Input book's ISBN"<<std::endl;
             std::cin>>index;
             borrowBook(lib, size, index);
         }
         else if(strcmp(t, "return") == 0)
         {
-            char index[20];
+            char index[20];     // searching isbn
             std::cout<<"Input book's ISBN"<<std::endl;
             std::cin>>index;
             returnBook(lib, size, index);
@@ -154,7 +180,7 @@ int main()
             break;
         }
         std::cout<<"input option (add , list , search , borrow , return or exit)"<<std::endl;
-        std::cin>>t;
+        std::cin>>t;        // next option
     }
     if(strcmp(t, "exit") == 0)
     {
